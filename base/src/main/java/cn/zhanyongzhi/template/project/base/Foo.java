5978c2d6331b6d6468940e634b2f481475b42225
湛永志 <yongzhi.zhan@gmail.com>
2017-04-25 22:46:22 +0800

    init

---
+++ base/src/main/java/cn/zhanyongzhi/template/project/base/Foo.java
@@ -0,0 +1,10 @@
+package cn.zhanyongzhi.template.project.base;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Foo {
+    public void run(){
+        System.out.println("Hello Foo");
+    }
+}
