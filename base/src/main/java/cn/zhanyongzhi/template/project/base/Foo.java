50129d07d141d1be6a77ae0466e92fede453ff38
湛永志 <yongzhi.zhan@gmail.com>
2017-04-25 23:13:00 +0800

    add dubbo interface

--- base/src/main/java/cn/zhanyongzhi/template/project/base/Foo.java
+++ base/src/main/java/cn/zhanyongzhi/template/project/base/Foo.java
@@ -1,9 +1,14 @@
 package cn.zhanyongzhi.template.project.base;
 
+import cn.zhanyongzhi.template.project.dubbointerface.IFoo;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Foo {
+public class Foo implements IFoo {
+    @Override
+    public String output(String val){
+        return "Foo " + val;
+    }
     public void run(){
         System.out.println("Hello Foo");
     }
